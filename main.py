504c1dfa48fbfe1e3f40b4c181894e8c11a56676
Brody234 <brodymassad@gmail.com>
2025-11-08 00:25:58 -0500

    Keystone, File Read/Write

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+import stat
+from keystone import Ks, KS_ARCH_ARM64, KS_MODE_LITTLE_ENDIAN
+
 class MemoryBlock:
     start: int
     length: int
@@ -6,9 +10,32 @@ class MemoryBlock:
         self.start = start
         self.length = length
 
+class StackVar:
+    start: int
+    length: int
+    scope: int
+    var_name: str
+
+    def __init__(self, start, length):
+        self.start = start
+        self.length = length
+
+FILE_NAME = "beta"
 int_registers = [None for _ in range(31)]
 float_registers = [None for _ in range(32)]
 
 stack_free_blocks = [MemoryBlock(0, None)]
 stack_list = []
 
+def process(line: str):
+    x=0
+    with open("beta", "w") as f:
+        f.write("")
+    
+
+with open("beta.tg") as f:
+    for line in f.readlines():
+        process(line)
+
+st = os.stat("beta")
+os.chmod("beta", st.st_mode | stat.S_IXUSR)
